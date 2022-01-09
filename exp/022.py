1bf77e665955b78618d1c17b5df55e19012e6992
osuossu8 <osuosuossu18@gmail.com>
2022-01-09 19:38:12 +0000

    022

--- exp/022.py
+++ exp/022.py
@@ -159,11 +159,12 @@ train.to_csv('input/train_folds_strat.csv', index=False)
 
 
 class Jigsaw4Dataset:
-    def __init__(self, df, cfg):
+    def __init__(self, df, cfg, tfidf):
         self.tokenizer = cfg.tokenizer
         self.max_len = cfg.max_len
         self.less_toxic = df['less_toxic'].values # .fillna("none").values
         self.more_toxic = df['more_toxic'].values # .fillna("none").values
+        self.tfidf_df = tfidf
 
     def __len__(self):
         return len(self.less_toxic)
@@ -193,6 +194,7 @@ class Jigsaw4Dataset:
         more_toxic_mask = more_toxic_inputs["attention_mask"]
         
         targets = torch.tensor(1, dtype=torch.float)
+        tfidf = self.tfidf_df.values[item]
 
         return {
             "less_input_ids": torch.tensor(less_toxic_ids, dtype=torch.long),
@@ -200,6 +202,7 @@ class Jigsaw4Dataset:
             "more_input_ids": torch.tensor(more_toxic_ids, dtype=torch.long),
             "more_attention_mask": torch.tensor(more_toxic_mask, dtype=torch.long),
             "targets" : torch.tensor(1, dtype=torch.float32),
+            "tfidf" : torch.tensor(tfidf, dtype=torch.float32),
         }
 
 
@@ -209,16 +212,24 @@ class RoBERTaBase(nn.Module):
         self.in_features = 768
         self.roberta = AutoModel.from_pretrained(model_path)
         self.dropout = nn.Dropout(0.2)
-        self.l0 = nn.Linear(self.in_features, 1)
+        self.process_tfidf = nn.Sequential(
+            nn.Linear(100, 32),
+            nn.LayerNorm(32),
+            nn.PReLU(),
+            nn.Dropout(0.1),
+        )
+        self.l0 = nn.Linear(self.in_features+32, 1)
 
-    def forward(self, ids, mask):
+    def forward(self, ids, mask, tfidf):
         roberta_outputs = self.roberta(
             ids,
             attention_mask=mask,
             output_hidden_states=False
         )
-        x = roberta_outputs[1]
-        logits = self.l0(self.dropout(x))
+        x1 = self.dropout(roberta_outputs[1])
+        x2 = self.process_tfidf(tfidf)
+        x = torch.cat([x1, x2], 1)
+        logits = self.l0(x)
         return logits.squeeze(-1)
 
 
@@ -241,9 +252,10 @@ def train_fn(model, data_loader, device, optimizer, scheduler):
         more_inputs = data['more_input_ids'].to(device)
         more_masks = data['more_attention_mask'].to(device)
         targets = data['targets'].to(device)
-        
-        less_toxic_logits = model(less_inputs, less_masks)
-        more_toxic_logits = model(more_inputs, more_masks)
+        tfidf = data['tfidf'].to(device)
+       
+        less_toxic_logits = model(less_inputs, less_masks, tfidf)
+        more_toxic_logits = model(more_inputs, more_masks, tfidf)
 
         loss = loss_fn(more_toxic_logits, less_toxic_logits, targets)
         loss.backward()
@@ -268,9 +280,10 @@ def valid_fn(model, data_loader, device):
             more_inputs = data['more_input_ids'].to(device)
             more_masks = data['more_attention_mask'].to(device)
             targets = data['targets'].to(device)
+            tfidf = data['tfidf'].to(device)
    
-            less_toxic_logits = model(less_inputs, less_masks)
-            more_toxic_logits = model(more_inputs, more_masks)
+            less_toxic_logits = model(less_inputs, less_masks, tfidf)
+            more_toxic_logits = model(more_inputs, more_masks, tfidf)
 
             loss = loss_fn(more_toxic_logits, less_toxic_logits, targets)
             losses.update(loss.item(), less_inputs.size(0))
@@ -291,13 +304,31 @@ def calc_cv(model_paths):
     df = pd.read_csv("input/train_folds_strat.csv")
     df = df.reset_index()
 
+    TP = TextPreprocessor()
+    preprocessed_text = TP.preprocess(pd.concat([df['more_toxic'], df['less_toxic']], 0))
+
+    pipeline = make_pipeline(
+                TfidfVectorizer(max_features=100000),
+                make_union(
+                    TruncatedSVD(n_components=50, random_state=42),
+                    make_pipeline(
+                        BM25Transformer(use_idf=True, k1=2.0, b=0.75),
+                        TruncatedSVD(n_components=50, random_state=42)
+                    ),
+                    n_jobs=1,
+                ),
+             )
+
+    z = pipeline.fit_transform(preprocessed_text)
+    tfidf_df = pd.DataFrame(z, columns=[f'cleaned_excerpt_tf_idf_svd_{i}' for i in range(50*2)])
+
     y_more = []
     y_less = []
     idx = []
     for fold, model in enumerate(models):
         val_df = df[df.kfold == fold].reset_index(drop=True)
     
-        dataset = Jigsaw4Dataset(df=val_df, cfg=CFG)
+        dataset = Jigsaw4Dataset(df=val_df, cfg=CFG, tfidf=tfidf_df)
         data_loader = torch.utils.data.DataLoader(
             dataset, batch_size=CFG.valid_bs, num_workers=0, pin_memory=True, shuffle=False
         )
@@ -310,9 +341,10 @@ def calc_cv(model_paths):
                 less_masks = data['less_attention_mask'].to(device)
                 more_inputs = data['more_input_ids'].to(device)
                 more_masks = data['more_attention_mask'].to(device)
+                tfidf = data['tfidf'].to(device)
 
-                less_toxic_logits = model(less_inputs, less_masks)
-                more_toxic_logits = model(more_inputs, more_masks)
+                less_toxic_logits = model(less_inputs, less_masks, tfidf)
+                more_toxic_logits = model(more_inputs, more_masks, tfidf)
                 
                 more_toxic_logits = more_toxic_logits.detach().cpu().numpy().tolist()
                 less_toxic_logits = less_toxic_logits.detach().cpu().numpy().tolist()
@@ -338,6 +370,116 @@ def calc_cv(model_paths):
     print(oof_df.shape)
 
 
+import nltk
+import re
+import scipy as sp
+from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.utils.validation import check_is_fitted
+from sklearn.feature_extraction.text import _document_frequency
+from sklearn.pipeline import make_pipeline, make_union
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.decomposition import TruncatedSVD
+
+
+class BM25Transformer(BaseEstimator, TransformerMixin):
+    def __init__(self, use_idf=True, k1=2.0, b=0.75):
+        self.use_idf = use_idf
+        self.k1 = k1
+        self.b = b
+
+    def fit(self, X):
+        if not sp.sparse.issparse(X):
+            X = sp.sparse.csc_matrix(X)
+        if self.use_idf:
+            n_samples, n_features = X.shape
+            df = _document_frequency(X)
+            idf = np.log((n_samples - df + 0.5) / (df + 0.5))
+            self._idf_diag = sp.sparse.spdiags(idf, diags=0, m=n_features, n=n_features)
+
+        doc_len = X.sum(axis=1)
+        self._average_document_len = np.average(doc_len)
+
+        return self
+
+    def transform(self, X, copy=True):
+        if hasattr(X, 'dtype') and np.issubdtype(X.dtype, np.float):
+            X = sp.sparse.csr_matrix(X, copy=copy)
+        else:
+            X = sp.sparse.csr_matrix(X, dtype=np.float, copy=copy)
+
+        n_samples, n_features = X.shape
+        doc_len = X.sum(axis=1)
+        sz = X.indptr[1:] - X.indptr[0:-1]
+        rep = np.repeat(np.asarray(doc_len), sz)
+
+        nom = self.k1 + 1
+        denom = X.data + self.k1 * (1 - self.b + self.b * rep / self._average_document_len)
+        data = X.data * nom / denom
+
+        X = sp.sparse.csr_matrix((data, X.indices, X.indptr), shape=X.shape)
+
+        if self.use_idf:
+            check_is_fitted(self, '_idf_diag', 'idf vector is not fitted')
+
+            expected_n_features = self._idf_diag.shape[0]
+            if n_features != expected_n_features:
+                raise ValueError("Input has n_features=%d while the model"
+                                 " has been trained with n_features=%d" % (
+                                     n_features, expected_n_features))
+            X = X * self._idf_diag
+
+        return X 
+
+
+class TextPreprocessor(object):
+    def __init__(self):
+        self.puncts = [',', '.', '"', ':', ')', '(', '-', '!', '?', '|', ';', "'", '$', '&', '/', '[', ']', '>', '%', '=', '#', '*', '+', '\\', '•',  '~', '@', '£',
+                       '·', '_', '{', '}', '©', '^', '®', '`',  '<', '→', '°', '€', '™', '›',  '♥', '←', '×', '§', '″', '′', 'Â', '█', '½', 'à', '…',
+                       '“', '★', '”', '–', '●', 'â', '►', '−', '¢', '²', '¬', '░', '¶', '↑', '±', '¿', '▾', '═', '¦', '║', '―', '¥', '▓', '—', '‹', '─',
+                       '▒', '：', '¼', '⊕', '▼', '▪', '†', '■', '’', '▀', '¨', '▄', '♫', '☆', 'é', '¯', '♦', '¤', '▲', 'è', '¸', '¾', 'Ã', '⋅', '‘', '∞', '«',
+                       '∙', '）', '↓', '、', '│', '（', '»', '，', '♪', '╩', '╚', '³', '・', '╦', '╣', '╔', '╗', '▬', '❤', 'ï', 'Ø', '¹', '≤', '‡', '√', '（', '）', '～',
+                       '➡', '％', '⇒', '▶', '「', '➄', '➆',  '➊', '➋', '➌', '➍', '⓪', '①', '②', '③', '④', '⑤', '⑰', '❶', '❷', '❸', '❹', '❺', '❻', '❼', '❽',  
+                       '＝', '※', '㈱', '､', '△', '℮', 'ⅼ', '‐', '｣', '┝', '↳', '◉', '／', '＋', '○',
+                       '【', '】', '✅', '☑', '➤', 'ﾞ', '↳', '〶', '☛', '｢', '⁺', '『', '≫',
+                       ]
+
+        self.numbers = ["0","1","2","3","4","5","6","7","8","9","０","１","２","３","４","５","６","７","８","９"]
+        self.stopwords = nltk.corpus.stopwords.words('english')
+
+    def _pre_preprocess(self, x):
+        return str(x).lower() 
+
+    def rm_num(self, x, use_num=True):
+        x = re.sub('[0-9]{5,}', '', x)
+        x = re.sub('[0-9]{4}', '', x)
+        x = re.sub('[0-9]{3}', '', x)
+        x = re.sub('[0-9]{2}', '', x)    
+        for i in self.numbers:
+            x = x.replace(str(i), '')        
+        return x
+
+    def clean_puncts(self, x):
+        for punct in self.puncts:
+            x = x.replace(punct, '')
+        return x
+    
+    def clean_stopwords(self, x):
+        list_x = x.split()
+        res = []
+        for w in list_x:
+            if w not in self.stopwords:
+                res.append(w)
+        return ' '.join(res)
+
+    def preprocess(self, sentence):
+        sentence = sentence.fillna(" ")
+        sentence = sentence.map(lambda x: self._pre_preprocess(x))
+        sentence = sentence.map(lambda x: self.clean_puncts(x))
+        sentence = sentence.map(lambda x: self.clean_stopwords(x))
+        sentence = sentence.map(lambda x: self.rm_num(x))
+        return sentence
+
+
 def init_logger(log_file='train.log'):
     from logging import getLogger, INFO, FileHandler,  Formatter,  StreamHandler
     logger = getLogger(__name__)
@@ -354,6 +496,25 @@ def init_logger(log_file='train.log'):
 logger = init_logger(log_file='log/' + f"{CFG.EXP_ID}.log")
 
 
+TP = TextPreprocessor()
+preprocessed_text = TP.preprocess(pd.concat([train['more_toxic'], train['less_toxic']], 0))
+
+pipeline = make_pipeline(
+                TfidfVectorizer(max_features=100000),
+                make_union(
+                    TruncatedSVD(n_components=50, random_state=42),
+                    make_pipeline(
+                        BM25Transformer(use_idf=True, k1=2.0, b=0.75),
+                        TruncatedSVD(n_components=50, random_state=42)
+                    ),
+                    n_jobs=1,
+                ),
+             )
+
+z = pipeline.fit_transform(preprocessed_text)
+tfidf_df = pd.DataFrame(z, columns=[f'cleaned_text_tf_idf_svd_{i}' for i in range(50*2)])
+
+
 # main loop
 for fold in range(5):
     if fold not in CFG.folds:
@@ -367,12 +528,12 @@ for fold in range(5):
 
     model = RoBERTaBase(CFG.model_name)    
   
-    train_dataset = Jigsaw4Dataset(df=trn_df, cfg=CFG)
+    train_dataset = Jigsaw4Dataset(df=trn_df, cfg=CFG, tfidf=tfidf_df)
     train_dataloader = torch.utils.data.DataLoader(
         train_dataset, batch_size=CFG.train_bs, num_workers=0, pin_memory=True, shuffle=True
     )
     
-    valid_dataset = Jigsaw4Dataset(df=val_df, cfg=CFG)
+    valid_dataset = Jigsaw4Dataset(df=val_df, cfg=CFG, tfidf=tfidf_df)
     valid_dataloader = torch.utils.data.DataLoader(
         valid_dataset, batch_size=CFG.valid_bs, num_workers=0, pin_memory=True, shuffle=False
     )
@@ -381,8 +542,6 @@ for fold in range(5):
     scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, eta_min=CFG.ETA_MIN, T_max=500)
 
     model = model.to(device)
-    model.load_state_dict(torch.load(f'output/020/fold-{fold}.bin'))
-    print('pretrained model loaded ...')
 
     min_loss = 999
     best_score = 0. # np.inf
