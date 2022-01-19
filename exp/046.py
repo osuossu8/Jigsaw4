a08066c032d7556b7c7ee36588ec12599dc6361d
osuossu8 <osuosuossu18@gmail.com>
2022-01-19 00:35:03 +0000

    046

--- exp/046.py
+++ exp/046.py
@@ -216,7 +216,7 @@ print(test.shape, submission.shape)
 # ====================================================
 
 # train = train[train['y']>0.2].reset_index(drop=True)
-train = train[train['y']>0].reset_index(drop=True)
+train = train[train['y']>0.1].reset_index(drop=True)
 print(train.shape)
 print(train['y'].value_counts())
 
