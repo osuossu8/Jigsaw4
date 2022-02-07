554ad5960eb41fd9476d8575fbeaea0e9e4bcdf3
osuossu8 <osuosuossu18@gmail.com>
2022-02-07 03:25:09 +0000

    082

--- exp/082.py
+++ exp/082.py
@@ -55,7 +55,7 @@ class CFG:
     train_bs = 64 # 32
     valid_bs = 128
     log_interval = 150
-    model_name = 'gpt2' # 'roberta-base'
+    model_name = 'facebook/bart-base' # 'roberta-base'
     EARLY_STOPPING = True
     DEBUG = False # True
     margin = 0.5
